e2b8f49cbbe4f39379fd6690c33c60a882d00cb9
Fan Wu <FW@AME.nu>
2025-12-03 18:26:21 +0100

    FMEA JSON Embedding test

--- Semantic_search_demo/json_loader.py
+++ Semantic_search_demo/json_loader.py
@@ -28,16 +28,16 @@ def load_json(file_path:str):
     print(f"[INFO] Loaded {len(docs)} FMEA entries from {file_path}")
     return docs
 
+# To be finished
 def create_embeddings(model_name="sentence-transformers/all-MiniLM-L6-v2"):
     embeddings = HuggingFaceEmbeddings(
         model_name=model_name,
-        model_kwargs={"device": "cpu"}  # change to cuda if GPU
+        model_kwargs={"device": "cpu"}
     )
     print("[INFO] Embeddings model initialized.")
     return embeddings
 
-def vector_store(embeddings, docs, persist_dir="../../../DATA/chroma_langchain_fmea_db"):
-    """Initialize Chroma vector store."""
+def create_vector_store(embeddings, persist_dir="../../../DATA/chroma_langchain_fmea_db"):
     vector_store = Chroma(
         collection_name="fmea_example_collection",
         embedding_function=embeddings,
@@ -47,17 +47,33 @@ def vector_store(embeddings, docs, persist_dir="../../../DATA/chroma_langchain_f
     return vector_store
 
 def add_failureblock_to_vector_store(vector_store, docs):
-    """Add document chunks to the vector store."""
-    ids = vector_store.add_texts(docs)
-    print(f"[INFO] Added {len(ids)} chunks to the vector store.")
+    """Add FMEA Documents to vector store."""
+    ids = vector_store.add_documents(docs)
+    print(f"[INFO] Added {len(ids)} FMEA entries to the vector store.")
     return ids
 
 def main():
     file_path = "../dfmea_effect_flat.json"
     docs = load_json(file_path)
-    print(docs[1])
-    print(docs[1].metadata)
+    # print(docs[1])
+    # print(docs[1].metadata)
 
+    # Create embedding model
+    embeddings = create_embeddings()
+
+    # Init vector store
+    vstore = create_vector_store(embeddings)
+
+    # Add all FMEA rows
+    add_failureblock_to_vector_store(vstore, docs)
+
+    query = "gateway backend incorrect state"
+    results = vstore.similarity_search(query, k=3)
+
+    for i, r in enumerate(results):
+        print(f"\n--- RESULT {i+1} ---")
+        print("CONTENT:", r.page_content)
+        print("METADATA:", r.metadata)
 
 
 if __name__ == "__main__":
